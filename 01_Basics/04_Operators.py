25cbe240fd26107ede9624ad24752537c1b008cd
Deep Kansara <deepkansara1212@gmail.com>
2025-05-29 10:35:06 +0530

    Operators

--- 01_Basics/04_Operators.py
+++ 01_Basics/04_Operators.py
@@ -12,3 +12,19 @@ a *= 2         # 10
 
 print(a is not b)
 print(a is b)
+
+
+# Operators Precedence & Associativity
+# Precedence: Determines the order of operations
+# Associativity: Determines the order of operations with the same precedence order
+
+# ()        -> left to right
+# / * %     -> left to right
+# + -       -> left to right
+
+Eq = 10 + (20 * 30) / 50 + 45 % 2 
+# Eq = 10 + 600 / 50 + 45 % 2
+# Eq = 10 + 12 + 45 % 2
+# Eq = 10 + 12 + 1
+# Eq = 23
+print(Eq)
