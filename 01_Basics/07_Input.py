15621afe4f691f7e1aebcb79002b775f39211e29
Deep Kansara <deepkansara1212@gmail.com>
2025-05-30 10:43:07 +0530

    User Input

---
+++ 01_Basics/07_Input.py
@@ -0,0 +1,12 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+age = int(age)
+
+
+# print("Hello ", name, "\nYou are ", age, " years old.")
+
+
+# f-string
+greeting = f"Hello {name}, You are {age} years old."
+print(greeting)
