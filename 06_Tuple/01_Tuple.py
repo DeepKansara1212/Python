2a382b5d6fc516957894b8f5c4c2e25c07a21ce9
Deep Kansara <deepkansara1212@gmail.com>
2025-06-10 10:40:20 +0530

    Tuples

---
+++ 06_Tuple/01_Tuple.py
@@ -0,0 +1,9 @@
+tuple1 = 10, 20, 30, 40
+
+tuple2 = (50, 60, 70, 80)
+
+print(tuple1)
+print(tuple2[1])  
+
+tuple3 = tuple("python")
+print(tuple3) 
---
+++ 06_Tuple/02_Tuple.py
@@ -0,0 +1,8 @@
+t1 = (10) 
+print(type(t1)) 
+
+t2 = (10,)
+print(type(t2)) 
+
+
+print(dir(tuple))
