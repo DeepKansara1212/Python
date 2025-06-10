106a3a86852668c2cdf84a9903027994f8781565
Deep Kansara <deepkansara1212@gmail.com>
2025-06-10 10:40:00 +0530

    Cloning

---
+++ 05_List/06_Cloning.py
@@ -0,0 +1,14 @@
+P = [10, 20, 30, 40, 50] 
+print("List of P: ", P)
+
+Q = P[:]
+
+print("After aliasing, Value of Q: ", Q) 
+
+
+print("------------------------------------------------")
+
+Q[2] = 'Python'
+
+print("P: ", P)
+print("Q: ", Q) 
